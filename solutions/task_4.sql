9e637ce5dbeed318a8f7ac4a16bd02c81f045b52
Levi Perelman <levi2p@gmail.com>
2023-04-05 01:55:56 +0300

    Better answer for task 4.

--- solutions/task_4.sql
+++ solutions/task_4.sql
@@ -1,15 +1,20 @@
-select year, sum(status)
+select
+  h2.yr as year,
+  sum(is_white_christmas) as white_christmas_count
 from (
   select
-    h2.yr as year,
-    (case when min(h1.m12) < 0 then 1 else 0 end) as status
-  from hadcet as h1
-  join hadcet as h2
-  on h1.yr between h2.yr + 2 and h2.yr + 11
-  where h1.dy between 21 and 25
-  group by h2.yr, h1.yr
-) as t_inner
-where year between 1809 and 1813
-group by year
+    yr,
+    (case when min(m12) < 0 then 1 else 0 end) as is_white_christmas
+  from hadcet
+  where dy between 21 and 25
+  group by yr
+) as h1
+join (
+  select distinct yr
+  from hadcet
+  where yr between 1805 and 1813
+) as h2
+on h1.yr between h2.yr + 2 and h2.yr + 11
+group by h2.yr
 order by year
 ;
